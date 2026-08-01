32516dd3ab55e8c1df10a9686964a90b46ff3af0
rodrigo-gr-pereira <rodrigogr.pereira@gmail.com>
2026-08-01 20:30:59 -0300

    RF01

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log(`
+===============================
+SISTEMA DE BIBLIOTECA
+===============================
+1 - Listar livros
+2 - Buscar livro
+3 - Listar livros disponíveis
+4 - Cadastrar livro
+5 - Realizar empréstimo
+6 - Realizar devolução
+7 - Exibir estatísticas
+0 - Sair
+`);

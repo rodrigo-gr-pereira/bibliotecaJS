27176f1e45dec8bc646766aa1cbce9f06119c290
rodrigo-gr-pereira <rodrigogr.pereira@gmail.com>
2026-08-03 19:20:49 -0300

    RF08 = exibir estatisitcas

--- realizarDevolucao.js
+++ realizarDevolucao.js
@@ -1,15 +1,19 @@
-﻿import { dadosLivros } from "./dadosLivros.js";
+import { dadosLivros } from "./dadosLivros.js";
 
 function realizarDevolucao(titulo) {
+    // Busca o livro no array pelo título
     const livroEncontrado = dadosLivros.find(livro => livro.titulo.toLowerCase() === titulo.toLowerCase());
 
+    // Caso o livro exista
     if (livroEncontrado) {
-        livroEncontrado.disponivel = true;
+        livroEncontrado.disponivel = true; // Altera a disponibilidade para true
         console.log("Livro devolvido com sucesso!");
         return;
     }
 
+    // Caso contrário (livro não existe no sistema)
     console.log("Livro não encontrado!");
 }
 
+// Exporta a função para ser usada no menu inicial
 export { realizarDevolucao };
